f4ad4bd9b6ee7160f73647d3213f3dd4400c86fd
Sumanth <sumanth.a@digiotai.com>
2023-09-01 23:50:44 +0530

    Updated URLs to work for all KPIS

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('FileUpload', views.uploadFile, name='FileUpload'),
-    path('getData', views.getData, name='getData')
+    path('<str:UploadType>/FileUpload', views.uploadFile, name='FileUpload'),
+    path('<str:DownoladType>/getData', views.getData, name='getData')
 ]
