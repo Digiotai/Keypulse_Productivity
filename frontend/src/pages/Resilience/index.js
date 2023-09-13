fd597b3b78e74b7d16a35fbbc39b7a662f267224
Sainath Guraka <sainath.g@digiotai.com>
2023-09-13 09:55:52 +0530

    inference changes

--- frontend/src/pages/Resilience/index.js
+++ frontend/src/pages/Resilience/index.js
@@ -47,7 +47,7 @@ export const Resilience = () => {
                             {show && <div className='card p-2' style={{ position: 'absolute', marginLeft: "-300px", marginTop: "-20px", zIndex: 9999, width: '250px' }}>
                                 {label === "inf" && <div>
                                     {getTitle("Inferences", "#427ae3", handleClose)}
-                                    <>{data.inference?.length > 0 ? getData(data.inference, "#427ae3") : <li className='m-0 p-0' style={{ fontFamily: "poppins", fontWeight: 400, fontSize: '12px', width: '190px', listStyle: 'none' }}>No Inferences Found!</li>}</>
+                                    <>{data.inference?.length > 0 ? getData(data.inference[0], "#427ae3") : <li className='m-0 p-0' style={{ fontFamily: "poppins", fontWeight: 400, fontSize: '12px', width: '190px', listStyle: 'none' }}>No Inferences Found!</li>}</>
                                 </div>}
                                 {label === "rec" && <div>
                                     {getTitle("Recommendations", "#800080", handleClose)}
