b74ad049b9a7422fd7f0eadf2ff1404c8af859d0
Sumanth <sumanth.a@digiotai.com>
2023-09-17 23:25:58 +0530

    removed redundant words from sustainability inference

--- backend/views.py
+++ backend/views.py
@@ -114,9 +114,9 @@ def getSustainabilityInference(df, file):
 
         i2 = df.sort_values(by=col).reset_index(drop=True)
         if file.startswith('kpEnergy'):
-            inftemp = f'{col} Energy was lower in {i2.loc[0, "Month"]}, {i2.loc[1, "Month"]}'
+            inftemp = f'{col} was lower in {i2.loc[0, "Month"]}, {i2.loc[1, "Month"]}'
         elif file.startswith('kpWaste'):
-            inftemp = f'Congratulations, {col} waste produced by each machine was lower in {i2.loc[0, "Month"]}, {i2.loc[1, "Month"]}'
+            inftemp = f'Congratulations, {col} produced by each machine was lower in {i2.loc[0, "Month"]}, {i2.loc[1, "Month"]}'
         elif file.startswith('kpPlantation'):
             inftemp = f'Plantation was lower in {i2.loc[0, "Month"]}, {i2.loc[1, "Month"]}'
         elif file.startswith('kpWater'):
@@ -128,7 +128,7 @@ def getSustainabilityInference(df, file):
 
         if i2.iloc[-1, 1] > avgplanned:
             if file.startswith('kpWaste'):
-                inftemp += f'.Attention, {col} waste produced by each machine was higher in {i2.iloc[-1, 0]}'
+                inftemp += f'.Attention, {col} produced by each machine was higher in {i2.iloc[-1, 0]}'
             elif file.startswith('kpco2'):
                 inftemp += f'. Attention, Co2 emission was higher in {i2.iloc[-1, 0]}'
             else:
